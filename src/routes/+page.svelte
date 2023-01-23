0fe44e9ff37f6cc7fc73232c962a7484beeeeb60
adhptrh <70842975+adhptrh@users.noreply.github.com>
2023-01-24 01:25:15 +0700

    fixed height on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,29 +3,27 @@
 	import '../app.css';
     import InputBox from "../lib/InputBox.svelte"
 	import Title from '../lib/Title.svelte';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const documentHeight = () => {
+			
+			const doc = document.documentElement
+			doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+		}
+		window.addEventListener('resize', documentHeight)
+		documentHeight()
+	})
 </script>
 
 <svelte:head>
 	<title>Anonymous Voice Discord Bot</title>
 </svelte:head>
 
-<div class="flex justify-center h-screen fix100vh bg-discord-bg-5 ">
-	<div class="flex flex-col h-screen fix100vh w-screen fixed bg-discord-bg-3 lg:max-w-2xl shadow-md">
+<div class="flex justify-center h-screen bg-discord-bg-5 ">
+	<div class="flex flex-col h-full w-full fixed bg-discord-bg-3 lg:max-w-2xl shadow-md">
 		<Title/>
 		<ChatBox/>
 		<InputBox/>
 	</div>
 </div>
-
-<style>
-	body {
-		min-height: 100vh;
-		min-height: -webkit-fill-available;
-	}
-	html {
-		height: -webkit-fill-available;
-	}
-	.fix100vh {
-		min-height: -webkit-fill-available;
-	}
-</style>
